91698cebe605ea953c49cb677cb7b933d2abf387
rsmgcp <rs.mgcp@gmail.com>
2018-07-05 22:16:25 +0900

    Initial commit

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+
+import Aux from '../../hoc/Aux';
+import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7
+}
+
+class BurgerBuilder extends Component {
+    state = {
+        ingredients: {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
+        },totalePrice: 4
+    }
+
+    addIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        console.log(oldCount[this.type] + 'from oldCount');
+        
+        const updateCount = oldCount + 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = updateCount;
+        const priceAddition = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalePrice;
+        const newPrice = oldPrice + priceAddition;
+        this.setState({totalePrice: newPrice, ingredients: updatedIngredients});
+    }
+
+    removeIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        if (oldCount <= 0) {
+            return;
+        }
+        const updateCount = oldCount - 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = updateCount;
+        const priceDeduction = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalePrice;
+        const newPrice = oldPrice - priceDeduction;
+        this.setState({totalePrice: newPrice, ingredients: updatedIngredients});
+    }
+    
+    render() {
+
+        return (
+            <Aux>
+                <Burger ingredients={this.state.ingredients} />
+                <BuildControls
+                    ingredientAdded={this.addIngredientHandler}
+                    ingredientRemove={this.removeIngredientHandler} />
+            </Aux>
+        );
+    }
+}
+
+export default BurgerBuilder;
