5319f341b32d19ac54dfde58d27b1d331e8371f4
@rsmgcp <rs.mgcp@gmail.com>
2018-08-08 19:11:40 +0900

    finished Route & fetch firebase

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,18 +1,16 @@
 import React from 'react';
-// import { withRouter } from 'react-router-dom';
 
 import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
-const burger = (props) => {
+const burger = ( props ) => {
     console.log(props);
-    
-    let transformedIngredients = Object.keys(props.ingredients)
-        .map(igKey => {
-            return [...Array(props.ingredients[igKey])].map((_, i) => {
+    let transformedIngredients = Object.keys( props.ingredients )
+        .map( igKey => {
+            return [...Array( props.ingredients[igKey] )].map( ( _, i ) => {
                 return <BurgerIngredient key={igKey + i} type={igKey} />;
-            });
-        })
+            } );
+        } )
         .reduce((arr, el) => {
             return arr.concat(el)
         }, []);
@@ -28,5 +26,4 @@ const burger = (props) => {
     );
 };
 
-// export default withRouter(burger);
 export default burger;
